bff1c167e0d5e871b2e7c6529feffc02f75fab62
Arshd <arshad@sort6.com>
2018-01-16 22:43:11 +0200

    updating files

--- fsAppend.js
+++ fsAppend.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
 
-fs.appendFile("textfile1.txt", "Hello Random Reader?", function(err) {
+// fs.appendFile("textfile1.txt", "Hello Random Reader?", function(err) {
+//     if(err) throw err;
+//     console.log("appended a file?!");
+// });
+
+fs.appendFile("textfile1.txt", "\n what are you doing this evening >.>", function(err) {
     if(err) throw err;
-    console.log("appended a file?!");
+    console.log("appended a file after creating it, niceeeee?!");
 });
--- fsWrite.js
+++ fsWrite.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
 
-fs.writeFile("textfile3.txt", "WRiTing File?", function(err) {
+// fs.writeFile("textfile3.txt", "WRiTing File?", function(err) {
+//     if(err) throw err;
+//     console.log("WROTE a file, FUDGE YEAH?!");
+// });
+
+fs.writeFile("textfile3.txt", "OVERWrItInG fILe$, Co0l", function(err) {
     if(err) throw err;
-    console.log("WROTE a file, FUDGE YEAH?!");
+    console.log("Over and replaced!");
 });
